055e213f74380143af650e943a94693e7efd099f
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-21 20:49:56 +0530

    refactor(server): api code splitting

---
+++ server/src/api.ts
@@ -0,0 +1,85 @@
+import { Elysia, NotFoundError, t } from "elysia";
+import { eq } from "drizzle-orm";
+import { todos } from "./db/schema";
+import { db } from "./db";
+
+const TodoDTO = t.Object({
+  title: t.String(),
+  completed: t.Optional(t.Boolean()),
+});
+
+const WithIdDTO = t.Object({
+  id: t.String(),
+});
+
+export const api = new Elysia({ prefix: "/api" }).decorate("db", db);
+
+api.group("/todos", (router) =>
+  router
+    .get("/", async ({ db }) => await db.query.todos.findMany())
+    .get(
+      "/:id",
+      async ({ db, params }) => {
+        const [todo] = await db.query.todos.findMany({
+          where: eq(todos.id, params.id),
+          limit: 1,
+        });
+        if (!todo) throw new NotFoundError();
+        return todo;
+      },
+      {
+        params: WithIdDTO,
+      }
+    )
+    .post(
+      "/",
+      async ({ db, body }) =>
+        await db
+          .insert(todos)
+          .values({ id: crypto.randomUUID(), ...body })
+          .returning(),
+      {
+        body: TodoDTO,
+      }
+    )
+    .put(
+      "/:id",
+      async ({ db, params, body }) => {
+        let [todo] = await db.query.todos.findMany({
+          where: eq(todos.id, params.id),
+          limit: 1,
+        });
+        if (!todo) throw new NotFoundError();
+
+        [todo] = await db
+          .update(todos)
+          .set(body)
+          .where(eq(todos.id, params.id))
+          .returning();
+        return todo;
+      },
+      {
+        params: WithIdDTO,
+        body: t.Partial(TodoDTO),
+      }
+    )
+    .delete(
+      "/:id",
+      async ({ db, params }) => {
+        let [todo] = await db.query.todos.findMany({
+          where: eq(todos.id, params.id),
+          limit: 1,
+        });
+        if (!todo) throw new NotFoundError();
+
+        [todo] = await db
+          .delete(todos)
+          .where(eq(todos.id, params.id))
+          .returning();
+        return todo;
+      },
+      {
+        params: WithIdDTO,
+      }
+    )
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,8 @@
-import { Elysia, NotFoundError, t } from "elysia";
-import { env } from "./env";
-import { db } from "./db";
-import { eq } from "drizzle-orm";
-import { todos } from "./db/schema";
+import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import staticPlugin from "@elysiajs/static";
+import { env } from "./env";
+import { api } from "./api";
 
 const app = new Elysia()
   .use(cors())
@@ -12,87 +10,7 @@ const app = new Elysia()
     set.redirect = "/index.html";
   })
   .use(staticPlugin({ prefix: "/", assets: env.PUBLIC_DIR }))
-  .decorate("db", db);
-
-app.get("/todos", async ({ db }) => await db.query.todos.findMany());
-
-app.get(
-  "/todos/:id",
-  async ({ db, params }) => {
-    const [todo] = await db.query.todos.findMany({
-      where: eq(todos.id, params.id),
-      limit: 1,
-    });
-    if (!todo) throw new NotFoundError();
-    return todo;
-  },
-  {
-    params: t.Object({
-      id: t.String(),
-    }),
-  }
-);
-
-app.post(
-  "/todos",
-  async ({ db, body }) =>
-    await db
-      .insert(todos)
-      .values({ id: crypto.randomUUID(), ...body })
-      .returning(),
-  {
-    body: t.Object({
-      title: t.String(),
-      completed: t.Optional(t.Boolean()),
-    }),
-  }
-);
-
-app.put(
-  "/todos/:id",
-  async ({ db, params, body }) => {
-    let [todo] = await db.query.todos.findMany({
-      where: eq(todos.id, params.id),
-      limit: 1,
-    });
-    if (!todo) throw new NotFoundError();
-
-    [todo] = await db
-      .update(todos)
-      .set(body)
-      .where(eq(todos.id, params.id))
-      .returning();
-    return todo;
-  },
-  {
-    params: t.Object({
-      id: t.String(),
-    }),
-    body: t.Object({
-      title: t.Optional(t.String()),
-      completed: t.Optional(t.Boolean()),
-    }),
-  }
-);
-
-app.delete(
-  "/todos/:id",
-  async ({ db, params }) => {
-    let [todo] = await db.query.todos.findMany({
-      where: eq(todos.id, params.id),
-      limit: 1,
-    });
-    if (!todo) throw new NotFoundError();
-
-    [todo] = await db.delete(todos).where(eq(todos.id, params.id)).returning();
-    return todo;
-  },
-  {
-    params: t.Object({
-      id: t.String(),
-    }),
-  }
-);
+  .use(api);
 
 app.listen(env.PORT, () => {
   console.log(
