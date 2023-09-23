6f2c16c42708778e5a4357bc9450ae129230435b
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-23 17:47:33 +0530

    feat(client): sort and group todos

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -2,13 +2,43 @@ import { useTodoStore } from "../shared/todos.store";
 import TodoListItem from "./TodoListItem";
 
 export default function TodoList() {
-  const todos = useTodoStore((state) => state.todos);
+  const todos = useTodoStore((state) =>
+    state.todos.sort((a, b) => b.createdAt - a.createdAt)
+  );
 
-  return (
-    <ul className="flex flex-col gap-2">
-      {todos.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
+  const pendingTodos = todos.filter((todo) => !todo.completed);
+  const completedTodos = todos.filter((todo) => todo.completed);
+
+  return todos.length > 0 ? (
+    <>
+      {pendingTodos.length > 0 && (
+        <section className="flex flex-col gap-2">
+          <h3 className="text-slate-500 text-sm font-medium ml-auto">
+            Pending
+          </h3>
+          <ul className="flex flex-col gap-2">
+            {pendingTodos.map((todo) => (
+              <TodoListItem key={todo.id} todo={todo} />
+            ))}
+          </ul>
+        </section>
+      )}
+      {completedTodos.length > 0 && (
+        <section className="flex flex-col gap-2">
+          <h3 className="text-slate-500 text-sm font-medium ml-auto">
+            Completed
+          </h3>
+          <ul className="flex flex-col gap-2">
+            {completedTodos.map((todo) => (
+              <TodoListItem key={todo.id} todo={todo} />
+            ))}
+          </ul>
+        </section>
+      )}
+    </>
+  ) : (
+    <p className="text-slate-500 text-center rounded-xl border-slate-400/40 border-2 p-3">
+      No Todos
+    </p>
   );
 }
