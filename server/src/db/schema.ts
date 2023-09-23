34f6514dff169a1f872a010da69a403ed0dcdfe7
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-23 17:35:02 +0530

    feat(server): add createdAt for todo

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,7 +1,11 @@
+import { sql } from "drizzle-orm";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const todos = sqliteTable("todos", {
   id: text("id").primaryKey(),
   title: text("title").notNull(),
   completed: integer("completed", { mode: "boolean" }).notNull().default(false),
+  createdAt: integer("created_at")
+    .notNull()
+    .default(sql`(strftime('%s', 'now'))`),
 });
