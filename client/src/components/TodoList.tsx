1d12f6a90f6c0206fa088f14d9931cff7b49424c
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-23 17:14:42 +0530

    refactor(client): select todos directly

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -2,11 +2,11 @@ import { useTodoStore } from "../shared/todos.store";
 import TodoListItem from "./TodoListItem";
 
 export default function TodoList() {
-  const todoStore = useTodoStore();
+  const todos = useTodoStore((state) => state.todos);
 
   return (
     <ul className="flex flex-col gap-2">
-      {todoStore.todos.map((todo) => (
+      {todos.map((todo) => (
         <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
