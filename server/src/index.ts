5de47368bf42aaeeaa2752c3f715324ab8bf89ea
tanishqmanuja <tanishqmanuja@gmail.com>
2023-09-28 16:59:11 +0530

    refactor(server): app.ts and export types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,10 @@
-import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
-import staticPlugin from "@elysiajs/static";
-import { swagger } from "@elysiajs/swagger";
+import app from "./app";
 import { env } from "./env";
-import { api } from "./api";
-
-const app = new Elysia()
-  .use(cors())
-  .use(
-    swagger({
-      documentation: {
-        info: {
-          title: "Todos API Documentation",
-          description: "Elysia BunJS Todos API",
-          version: "1.0.0",
-        },
-        tags: [{ name: "Todos", description: "Todos endpoints" }],
-      },
-      exclude: ["/"],
-    })
-  )
-  .get("/", ({ set }) => {
-    set.redirect = "/index.html";
-  })
-  .use(staticPlugin({ prefix: "/", assets: env.PUBLIC_DIR }))
-  .use(api);
 
 app.listen(env.PORT, () => {
   console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
   );
 });
+
+export type App = typeof app;
