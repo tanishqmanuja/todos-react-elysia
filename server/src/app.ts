28caa5b7811661dc19c4904f77a9ef25f68d56c8
tanishqmanuja <tanishqmanuja@gmail.com>
2024-08-31 14:57:32 +0530

    feat: use nice logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,12 @@
 import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
+import { logger } from "@tqman/nice-logger";
 import { Elysia } from "elysia";
 
 import { api } from "./api";
-import { isProd } from "./env";
-import { logger } from "./lib/logger";
-import { pluginConditionally } from "./utils/elysia";
 
 export const app = new Elysia()
 	.use(cors())
 	.use(swagger())
-	.use(pluginConditionally(!isProd, logger))
+	.use(logger())
 	.use(api);
--- server/src/utils/elysia.ts
+++
@@ -1,8 +0,0 @@
-import type Elysia from "elysia";
-
-export function pluginConditionally<Plugin = never>(
-	condition: boolean,
-	plugin: Plugin,
-) {
-	return (app: Elysia) => (condition ? plugin : app);
-}
